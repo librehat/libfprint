a87f26f1765b3a5d93376b56b1661615a97c0a5f
chester <chester@pixelauth.com>
2020-08-31 17:59:19 +0800

    indent fix

--- libfprint/drivers/pixelauth/storage_helper.c
+++ libfprint/drivers/pixelauth/storage_helper.c
@@ -40,234 +40,231 @@
 #include "pixelauth.h"
 #include "storage_helper.h"
 
-const gchar* pa_description = "/dev/";
+const gchar * pa_description = "/dev/";
 
-gchar*
-get_pa_data_descriptor (FpPrint* print,
-                        FpDevice* self,
-                        gint dev_index)
+gchar * get_pa_data_descriptor (FpPrint  * print,
+                                FpDevice * self,
+                                gint       dev_index)
 {
-    const gchar* driver;
-    const gchar* dev_id;
+  const gchar * driver;
+  const gchar * dev_id;
 
-    if (print)
-    {
-        driver = fp_print_get_driver (print);
-        dev_id = fp_print_get_device_id (print);
-    }
-    else
-    {
-        driver = fp_device_get_driver (self);
-        dev_id = fp_device_get_device_id (self);
-    }
+  if (print)
+  {
+    driver = fp_print_get_driver (print);
+    dev_id = fp_print_get_device_id (print);
+  }
+  else
+  {
+    driver = fp_device_get_driver (self);
+    dev_id = fp_device_get_device_id (self);
+  }
 
-    return g_strdup_printf ("%s/%s/%x",
-                            driver,
-                            dev_id,
-                            dev_index);
+  return g_strdup_printf ("%s/%s/%x",
+                          driver,
+                          dev_id,
+                          dev_index);
 }
 
-GVariantDict*
-_load_data (void)
+GVariantDict * _load_data (void)
 {
-    GVariantDict* res;
-    GVariant* var;
-    gchar* contents = NULL;
-    gsize length = 0;
-    GError* err = NULL;
+  GVariantDict * res;
+  GVariant * var;
+  gchar * contents = NULL;
+  gsize length = 0;
+  GError * err = NULL;
 
-    if (!g_file_get_contents (STORAGE_FILE, &contents, &length, &err))
-    {
-        g_warning ("Error loading storage, assuming it is empty, message:%s\n", err->message);
-        return g_variant_dict_new (NULL);
-    }
+  if (!g_file_get_contents (STORAGE_FILE, &contents, &length, &err))
+  {
+      g_warning ("Error loading storage, assuming it is empty, message:%s\n", err->message);
+      return g_variant_dict_new (NULL);
+  }
 
-    var = g_variant_new_from_data (G_VARIANT_TYPE_VARDICT,
-                                   contents,
-                                   length,
-                                   FALSE,
-                                   g_free,
-                                   contents);
+  var = g_variant_new_from_data (G_VARIANT_TYPE_VARDICT,
+                                 contents,
+                                 length,
+                                 FALSE,
+                                 g_free,
+                                 contents);
 
-    res = g_variant_dict_new (var);
-    g_variant_unref (var);
-    return res;
+  res = g_variant_dict_new (var);
+  g_variant_unref (var);
+  return res;
 }
 
-gint
-_save_data (GVariant* data)
+gint _save_data (GVariant * data)
 {
-    const gchar* contents = NULL;
-    gsize length;
-    GError* err = NULL;
+  const gchar * contents = NULL;
+  gsize length;
+  GError * err = NULL;
 
-    length = g_variant_get_size (data);
-    contents = (gchar*)g_variant_get_data (data);
+  length = g_variant_get_size (data);
+  contents = ( gchar * )g_variant_get_data (data);
 
-    if (!g_file_set_contents (STORAGE_FILE, contents, length, &err))
-    {
-        g_warning ("Error saving storage, message:%s\n!", err->message);
-        return -1;
-    }
+  if (!g_file_set_contents (STORAGE_FILE, contents, length, &err))
+  {
+    g_warning ("Error saving storage, message:%s\n!", err->message);
+    return -1;
+  }
 
-    g_variant_ref_sink (data);
-    g_variant_unref (data);
+  g_variant_ref_sink (data);
+  g_variant_unref (data);
 
-    return 0;
+  return 0;
 }
 
-FpPrint*
-pa_data_load (FpDevice* self,
-              FpFinger finger,
-              const gchar* username,
-              gint db_count)
+FpPrint * pa_data_load (FpDevice    * self,
+                        FpFinger      finger,
+                        const gchar * username,
+                        gint          db_count)
 {
-    g_autofree gchar* descr = NULL;
-    g_autoptr (GVariant) val = NULL;
-    g_autoptr (GVariantDict) dict = NULL;
-    const guchar* stored_data = NULL;
-    gsize stored_len;
+  g_autofree gchar * descr = NULL;
+  g_autoptr (GVariant) val = NULL;
+  g_autoptr (GVariantDict) dict = NULL;
+  const guchar * stored_data = NULL;
+  gsize stored_len;
 
-    dict = _load_data ();
-    for (int i = 0; i < db_count; i++)
+  dict = _load_data ();
+  for (int i = 0; i < db_count; i++)
+  {
+    descr = get_pa_data_descriptor (NULL, self, i);
+    val = g_variant_dict_lookup_value (dict, descr, G_VARIANT_TYPE ("ay"));
+    if (val)
     {
-        descr = get_pa_data_descriptor (NULL, self, i);
-        val = g_variant_dict_lookup_value (dict, descr, G_VARIANT_TYPE ("ay"));
-        if (val)
-        {
-            FpPrint* print;
-            g_autoptr (GError) error = NULL;
+      FpPrint * print;
+      g_autoptr (GError) error = NULL;
 
-            stored_data = (const guchar*)g_variant_get_fixed_array (val, &stored_len, 1);
-            print = fp_print_deserialize (stored_data, stored_len, &error);
+      stored_data = ( const guchar * )g_variant_get_fixed_array (val, &stored_len, 1);
+      print = fp_print_deserialize (stored_data, stored_len, &error);
 
-            if (error)
-                g_warning ("Error deserializing data: %s", error->message);
+      if (error)
+        g_warning ("Error deserializing data: %s", error->message);
 
-            if (fp_print_get_finger (print) != finger)
-                continue;
+      if (fp_print_get_finger (print) != finger)
+        continue;
 
-            if (username)
-            {
-                if (g_strcmp0 (fp_print_get_username (print), username) == 0)
-                    return print;
-                else
-                    return NULL;
+      if (username)
+      {
+        if (g_strcmp0 (fp_print_get_username (print), username) == 0)
+          return print;
+        else
+          return NULL;
             }
-            return print;
+        return print;
         }
     }
-
-    return NULL;
+  return NULL;
 }
 
-gint pa_data_save (FpPrint* print, gint dev_index)
+gint pa_data_save (FpPrint * print, gint dev_index)
 {
-    g_autofree gchar* descr = get_pa_data_descriptor (print, NULL, dev_index);
+  g_autofree gchar * descr = get_pa_data_descriptor (print, NULL, dev_index);
 
-    g_autoptr (GError) error = NULL;
-    g_autoptr (GVariantDict) dict = NULL;
-    g_autofree guchar* data = NULL;
-    GVariant* val;
-    gsize size;
-    gint res;
+  g_autoptr (GError) error = NULL;
+  g_autoptr (GVariantDict) dict = NULL;
+  g_autofree guchar * data = NULL;
+  GVariant * val;
+  gsize size;
+  gint res;
 
-    dict = _load_data ();
+  dict = _load_data ();
 
-    fp_print_serialize (print, &data, &size, &error);
-    if (error)
-    {
-        g_warning ("Error serializing data: %s", error->message);
-        return -1;
-    }
-    val = g_variant_new_fixed_array (G_VARIANT_TYPE ("y"), data, size, 1);
-    g_variant_dict_insert_value (dict, descr, val);
+  fp_print_serialize (print, &data, &size, &error);
+  if (error)
+  {
+    g_warning ("Error serializing data: %s", error->message);
+    return -1;
+  }
+  val = g_variant_new_fixed_array (G_VARIANT_TYPE ("y"), data, size, 1);
+  g_variant_dict_insert_value (dict, descr, val);
 
-    res = _save_data (g_variant_dict_end (dict));
+  res = _save_data (g_variant_dict_end (dict));
 
-    return res;
+  return res;
 }
 
-gint pa_data_del (FpDevice* self,
-                  FpPrint* print,
-                  const gchar* username,
-                  gint db_count)
+gint pa_data_del (FpDevice    * self,
+                  FpPrint     * print,
+                  const gchar * username,
+                  gint          db_count)
 {
-    g_autofree gchar* descr = NULL;
+  g_autofree gchar * descr = NULL;
 
-    g_autoptr (GVariant) val = NULL;
-    g_autoptr (GVariantDict) dict = NULL;
-    const guchar* stored_data = NULL;
-    gsize stored_len;
+  g_autoptr (GVariant) val = NULL;
+  g_autoptr (GVariantDict) dict = NULL;
+  const guchar * stored_data = NULL;
+  gsize stored_len;
 
-    if (!username)
-        return PA_OK;
+  if (!username)
+    return PA_OK;
 
-    dict = _load_data ();
-    for (int i = 0; i < db_count; i++)
+  dict = _load_data ();
+  for (int i = 0; i < db_count; i++)
+  {
+    descr = get_pa_data_descriptor (print, self, i);
+    val = g_variant_dict_lookup_value (dict, descr, G_VARIANT_TYPE ("ay"));
+
+    if (val)
     {
-        descr = get_pa_data_descriptor (print, self, i);
-        val = g_variant_dict_lookup_value (dict, descr, G_VARIANT_TYPE ("ay"));
+      FpPrint * print_got;
+      g_autoptr (GError) error = NULL;
 
-        if (val)
-        {
-            FpPrint* print_got;
-            g_autoptr (GError) error = NULL;
+      stored_data = ( const guchar * )g_variant_get_fixed_array (val, &stored_len, 1);
+      print_got = fp_print_deserialize (stored_data, stored_len, &error);
 
-            stored_data = (const guchar*)g_variant_get_fixed_array (val, &stored_len, 1);
-            print_got = fp_print_deserialize (stored_data, stored_len, &error);
+      if (error)
+        g_warning ("Error deserializing data: %s", error->message);
 
-            if (error)
-                g_warning ("Error deserializing data: %s", error->message);
+      if (fp_print_get_finger (print) != fp_print_get_finger (print_got))
+        continue;
 
-            if (fp_print_get_finger (print) != fp_print_get_finger (print_got))
-                continue;
-
-            if (g_strcmp0 (fp_print_get_username (print_got), username) == 0)
-            {
-                g_variant_dict_remove (dict, descr);
-                _save_data (g_variant_dict_end (dict));
-                return PA_OK;
-            }
-        }
+      if (g_strcmp0 (fp_print_get_username (print_got), username) == 0)
+      {
+         g_variant_dict_remove (dict, descr);
+         _save_data (g_variant_dict_end (dict));
+         return PA_OK;
+      }
     }
+  }
 
     return PA_ERROR;
 }
 
-gint get_dev_index (FpDevice* self, FpPrint* print, gint db_count)
+gint get_dev_index (FpDevice * self,
+                    FpPrint  * print,
+                    gint       db_count)
 {
-    FpPrint* enroll_print = NULL;
-    enroll_print = pa_data_load (self, fp_print_get_finger (print), fp_print_get_username (print), db_count);
-    if (enroll_print == NULL)
-        return PA_ERROR;
-    const gchar* dev_str = fp_print_get_description (enroll_print);
-    fp_info ("get_dev_index %s \n", dev_str);
-    gint dev_index = dev_str[6] - 0x30; // /dev//[x]
-    return dev_index;
+  FpPrint * enroll_print = NULL;
+  enroll_print = pa_data_load (self, fp_print_get_finger (print), fp_print_get_username (print), db_count);
+  if (enroll_print == NULL)
+    return PA_ERROR;
+  const gchar * dev_str = fp_print_get_description (enroll_print);
+  fp_info ("get_dev_index %s \n", dev_str);
+  gint dev_index = dev_str[6] - 0x30; // /dev//[x]
+  return dev_index;
 }
 
 void
-gen_finger (gint dev_index, FpPrint* print)
+gen_finger (gint dev_index, FpPrint * print)
 {
-    GVariant* data = NULL;
-    GVariant* uid = NULL;
-    g_autoptr (GDate) date = NULL;
-    guint finger;
-    g_autofree gchar* user_id = fpi_print_generate_user_id (print);
-    gssize user_id_len = strlen (user_id);
+  GVariant * data = NULL;
+  GVariant * uid = NULL;
+  g_autoptr (GDate) date = NULL;
+  guint finger;
+  g_autofree gchar * user_id = fpi_print_generate_user_id (print);
+  gssize user_id_len = strlen (user_id);
 
-    finger = fp_print_get_finger (print);
-    uid = g_variant_new_fixed_array (G_VARIANT_TYPE_BYTE,
-                                     user_id,
-                                     user_id_len,
-                                     1);
-    data = g_variant_new ("(y@ay)", finger, uid);
-    fpi_print_set_type (print, FPI_PRINT_RAW);
-    fpi_print_set_device_stored (print, TRUE);
-    g_object_set (print, "fpi-data", data, NULL);
-    date = g_date_new ();
-    fp_print_set_enroll_date (print, date);
+  finger = fp_print_get_finger (print);
+  uid = g_variant_new_fixed_array (G_VARIANT_TYPE_BYTE,
+                                   user_id,
+                                   user_id_len,
+                                   1);
+  data = g_variant_new ("(y@ay)", finger, uid);
+  fpi_print_set_type (print, FPI_PRINT_RAW);
+  fpi_print_set_device_stored (print, TRUE);
+  g_object_set (print, "fpi-data", data, NULL);
+  date = g_date_new ();
+  fp_print_set_enroll_date (print, date);
 
-    g_object_set (print, "description", g_strdup_printf ("%s/%d", pa_description, dev_index), NULL);
+  g_object_set (print, "description", g_strdup_printf ("%s/%d", pa_description, dev_index), NULL);
 }
--- libfprint/drivers/pixelauth/storage_helper.h
+++ libfprint/drivers/pixelauth/storage_helper.h
@@ -2,32 +2,29 @@
 
 #define STORAGE_FILE "/usr/lib/fprintd/pa-storage.variant"
 
-gchar*
-get_pa_data_descriptor (FpPrint* print,
-                        FpDevice* self,
-                        gint dev_index);
+gchar * get_pa_data_descriptor (FpPrint  * print,
+                                FpDevice * self,
+                                gint       dev_index);
 
-GVariantDict*
-_load_data (void);
+GVariantDict * _load_data (void);
 
-gint
-_save_data (GVariant* data);
+gint _save_data (GVariant * data);
 
-FpPrint* pa_data_load (FpDevice* self,
-                       FpFinger finger,
-                       const gchar* username,
-                       gint db_count);
+FpPrint * pa_data_load (FpDevice    * self,
+                        FpFinger      finger,
+                        const gchar * username,
+                        gint          db_count);
 
-gint pa_data_save (FpPrint* print, gint dev_index);
+gint pa_data_save (FpPrint * print, gint dev_index);
 
-gint pa_data_del (FpDevice* self,
-                  FpPrint* print,
-                  const gchar* username,
-                  gint db_count);
+gint pa_data_del (FpDevice    * self,
+                  FpPrint     * print,
+                  const gchar * username,
+                  gint          db_count);
 
-gint get_dev_index (FpDevice* self,
-                    FpPrint* print,
-                    gint db_count);
+gint get_dev_index (FpDevice * self,
+                    FpPrint  * print,
+                    gint       db_count);
 
 void
-gen_finger (gint dev_index, FpPrint* print);
+gen_finger (gint dev_index, FpPrint * print);

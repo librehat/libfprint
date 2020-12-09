979aa10b210ac7db728186f8d59d090f0e99ce7b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2020-12-09 01:36:24 +0100

    goodixmoc: Add async identification test using on-owned deseralized prints
    This simulates what fprintd does

--- tests/goodixmoc/custom.py
+++ tests/goodixmoc/custom.py
@@ -19,8 +19,17 @@ d.open_sync()
 
 template = FPrint.Print.new(d)
 
+identified = False
+
 def enroll_progress(*args):
     print('enroll progress: ' + str(args))
+
+def identify_done(dev, res):
+    global identified
+    identified = True
+    identify_match, identify_print = dev.identify_finish(res)
+    print('async indentification done: ', identify_match, identify_print)
+    assert identify_match.equal(identify_print)
 
 # List, enroll, list, verify, identify, delete
 print("enrolling")
@@ -35,16 +44,33 @@ assert stored[0].equal(p)
 print("verifying")
 verify_res, verify_print = d.verify_sync(p)
 print("verify done")
+del p
 assert verify_res == True
-print("identifying")
-identify_match, identify_print = d.identify_sync(stored)
-print("identify done")
-assert identify_match.equal(identify_print)
+# print("identifying")
+# identify_match, identify_print = d.identify_sync(stored)
+# assert identify_match.equal(identify_print)
+# print("identify done")
+# del identify_match
+# del identify_print
+
+deserialized_prints = []
+for p in stored:
+    deserialized_prints.append(FPrint.Print.deserialize(p.serialize()))
+    assert deserialized_prints[-1].equal(p)
+del stored
+
+print('async identifying')
+d.identify(deserialized_prints, callback=identify_done)
+del deserialized_prints
+
+while not identified:
+    ctx.iteration(True)
 
 print("deleting")
 d.delete_print_sync(p)
 print("delete done")
-d.close_sync()
+
+assert d.close_sync() == True
 
 del d
 del c
